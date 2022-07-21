60194e8d58a3af264cdfc621fe90820c979e1c2d
youssouf ansfidine <ansfidine@hotmail.fr>
2022-07-21 23:47:34 +0800

    Implementation

--- PasswordEdit/Form1.Designer.cs
+++ PasswordEdit/Form1.Designer.cs
@@ -28,91 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
             this.buttonShowUser = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.buttonEdit = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxConsole
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 175);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(858, 358);
-            this.textBox1.TabIndex = 0;
+            this.textBoxConsole.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxConsole.Location = new System.Drawing.Point(16, 116);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.Size = new System.Drawing.Size(699, 207);
+            this.textBoxConsole.TabIndex = 0;
             // 
             // buttonShowUser
             // 
-            this.buttonShowUser.Location = new System.Drawing.Point(576, 47);
+            this.buttonShowUser.Location = new System.Drawing.Point(446, 47);
             this.buttonShowUser.Name = "buttonShowUser";
             this.buttonShowUser.Size = new System.Drawing.Size(121, 38);
             this.buttonShowUser.TabIndex = 1;
             this.buttonShowUser.Text = "Users";
             this.buttonShowUser.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(17, 122);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(469, 38);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 89);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Password";
+            this.buttonShowUser.Click += new System.EventHandler(this.buttonShowUser_Click);
             // 
             // buttonEdit
             // 
-            this.buttonEdit.Location = new System.Drawing.Point(723, 46);
+            this.buttonEdit.Location = new System.Drawing.Point(593, 46);
             this.buttonEdit.Name = "buttonEdit";
             this.buttonEdit.Size = new System.Drawing.Size(121, 38);
             this.buttonEdit.TabIndex = 1;
             this.buttonEdit.Text = "Edit";
             this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
             // 
-            // textBox3
+            // textBoxUsername
             // 
-            this.textBox3.Location = new System.Drawing.Point(17, 47);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(469, 38);
-            this.textBox3.TabIndex = 2;
+            this.textBoxUsername.Location = new System.Drawing.Point(17, 47);
+            this.textBoxUsername.Multiline = true;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(401, 38);
+            this.textBoxUsername.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 14);
+            this.label2.Location = new System.Drawing.Point(17, 14);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.Size = new System.Drawing.Size(92, 20);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Username";
+            this.label2.Text = "Username*";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 545);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(729, 343);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.buttonEdit);
             this.Controls.Add(this.buttonShowUser);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxConsole);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -124,12 +106,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxConsole;
         private System.Windows.Forms.Button buttonShowUser;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonEdit;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxUsername;
         private System.Windows.Forms.Label label2;
     }
 }
--- PasswordEdit/Form1.cs
+++ PasswordEdit/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Principal;
 
 namespace PasswordEdit
 {
     public partial class Form1 : Form
     {
+        public void ShowUsers()
+        {
+            ProcessStartInfo showUsers = new ProcessStartInfo("net","user");
+            showUsers.UseShellExecute = false;
+            showUsers.RedirectStandardOutput = true;
+            showUsers.CreateNoWindow = true;
+            var proc = Process.Start(showUsers);
+            string resultProcess = proc.StandardOutput.ReadToEnd();
+            textBoxConsole.Text=resultProcess;
+        }
+
+        public void EditPassword()
+        {
+            string arg =" user "+textBoxUsername.Text + " *";
+            string cmd = "net";
+            
+            ProcessStartInfo editPass = new ProcessStartInfo(cmd,arg);
+            editPass.UseShellExecute = false;
+            editPass.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            editPass.Verb = "runas";
+            var proc = Process.Start(editPass);
+            textBoxConsole.Text = "/n Edit password on process...."; 
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonShowUser_Click(object sender, EventArgs e)
+        {
+           ShowUsers();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            
+            EditPassword();
         }
     }
 }
